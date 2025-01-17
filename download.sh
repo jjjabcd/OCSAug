61f39e847f3ec188980292f5e6a06648f3216700
jjjabcd <148530193+jjjabcd@users.noreply.github.com>
2025-01-17 21:16:29 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -16,13 +16,16 @@ gdown https://drive.google.com/uc?id=1pfxuhMuXv5KIgW2blSG5kUNwyne5DtmW
 unzip image.zip
 rm image.zip
 
-
+mkdir -p test_mask_sample
+cd test_mask_sample/
 gdown https://drive.google.com/uc?id=1GL2gBE69DYVp118vJLKfWUgWh4KNaYd9
 unzip test_mask_sample.zip
 rm test_mask_sample.zip
+cd ..
 
+mkdir -p test_image_sample
+cd test_image_sample/
 gdown https://drive.google.com/uc?id=1w35ilYMA5KBD9KLbFlY_ORo7x2fdXyGx
 unzip test_image_sample.zip
 rm test_image_sample.zip
-
 )
